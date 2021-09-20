0a4a22a83866ae57386ebe99a4902022a0424646
Wesley Li <wesleyl@canva.com>
2021-09-20 22:06:08 +1000

    added ref search

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/hex"
 	"flag"
 	"fmt"
 	"path/filepath"
@@ -10,11 +9,13 @@ import (
 )
 
 func main() {
-	flag.Parse()
+	oidPtr := flag.String("oid", "", "oid to search")
+	refPtr := flag.String("ref", "", "ref to search")
 
+	flag.Parse()
 	args := flag.Args()
-	if len(args) != 2 {
-		fmt.Println("Usage: hello-go [dir] [oid]")
+	if len(args) != 1 {
+		fmt.Println("Usage: hello-go --oid --ref [dir]")
 		return
 	}
 
@@ -23,10 +24,22 @@ func main() {
 		fmt.Println("dir is invalid")
 		return
 	}
-	oid := args[1]
+	oid := *oidPtr
+	ref := *refPtr
 
-	fmt.Printf("Looking for %s in %s\n", oid, dir)
-	found, err := hasObject(dir, oid)
+	if oid != "" {
+		cmdOid(oid, dir)
+	} else if ref != "" {
+		cmdRef(ref, dir)
+	} else {
+		fmt.Println("Need either oid or ref")
+		return
+	}
+}
+
+func cmdOid(oid, dir string) {
+	fmt.Printf("Looking for oid %s in %s\n", oid, dir)
+	found, err := hasObject(oid, dir)
 	if err != nil {
 		fmt.Printf("ERR: %v\n", err)
 		return
@@ -38,21 +51,50 @@ func main() {
 	}
 }
 
-func hasObject(dir, oid string) (bool, error) {
+func hasObject(oidString, dir string) (bool, error) {
 	repo, err := git.OpenRepository(dir)
 	if err != nil {
-		return false, fmt.Errorf("Failed to open dir. Error: %v", err)
+		return false, fmt.Errorf("failed to open dir. Error: %v", err)
 	}
 
 	odb, err := repo.Odb()
 	if err != nil {
-		return false, fmt.Errorf("Failed to open odb. Error: %v", err)
+		return false, fmt.Errorf("failed to open odb. Error: %v", err)
 	}
 
-	oidb, err := hex.DecodeString(oid)
+	oid, err := git.NewOid(oidString)
 	if err != nil {
-		return false, fmt.Errorf("Failed to decode oid. Error: %v", err)
+		return false, fmt.Errorf("failed to decode oid. Error: %v", err)
 	}
 
-	return odb.Exists((*git.Oid)(oidb)), nil
+	return odb.Exists(oid), nil
+}
+
+func cmdRef(ref, dir string) {
+	fmt.Printf("Looking for ref %s in %s\n", ref, dir)
+	oid, err := ref2Oid(ref, dir)
+	if err != nil {
+		fmt.Printf("ERR: %v\n", err)
+		return
+	}
+	fmt.Println(oid)
+}
+
+func ref2Oid(refName, dir string) (string, error) {
+	repo, err := git.OpenRepository(dir)
+	if err != nil {
+		return "", fmt.Errorf("failed to open dir. Error: %v", err)
+	}
+
+	ref, err := repo.References.Lookup(refName)
+	if err != nil {
+		return "", fmt.Errorf("failed to lookup ref. Error: %v", err)
+	}
+
+	resolvedRef, err := ref.Resolve()
+	if err != nil {
+		return "", fmt.Errorf("failed to resolve ref. Error: %v", err)
+	}
+
+	return resolvedRef.Target().String(), nil
 }
